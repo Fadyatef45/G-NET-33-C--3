44c9b0492d96d68601671a72b2bdbcf571d3c1e0
Fadyatef45 <ateffady98@gmail.com>
2026-02-10 22:07:12 +0200

    question1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            #region question1
+
+            double d = 9.99;
+            int x = (int)d;
+            Console.WriteLine(x); // 9
+
+            //Explicit Casting
+
+            #endregion
+
+
         }
     }
 }
