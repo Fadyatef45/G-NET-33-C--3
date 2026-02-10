3897e36b12984bc08585e2b41fbd646443dcb17d
Fadyatef45 <ateffady98@gmail.com>
2026-02-10 22:11:29 +0200

    question1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,14 +8,15 @@
 
             #region question1
 
-            double d = 9.99;
-            int x = (int)d;
-            Console.WriteLine(x); // 9
+          ///  double d = 9.99;
+          ///  int x = (int)d;
+          ///  Console.WriteLine(x); // 9
 
             //Explicit Casting
 
             #endregion
 
+           
 
         }
     }
